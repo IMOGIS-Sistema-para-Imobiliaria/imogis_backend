2d8fe12fcab4b0407448ebaf576f0144cedd6aec
Jhonnatan Douglas <jhonnatanaraujodev@gmail.com>
2024-10-18 18:09:13 -0300

    feat: configurado o models de users

--- users/serializers.py
+++ users/serializers.py
@@ -8,22 +8,33 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = [
             "id",
+            "username",
             "password",
             "email",
-            "date_joined",
+            "reset_password",
             "is_superuser",
+            "is_active",
+            "date_joined",
         ]
         extra_kwargs = {
             "password": {"write_only": True},
         }
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict):
+        if validated_data["is_superuser"]:
+            return User.objects.create_superuser(**validated_data)
         return User.objects.create_user(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: User, validated_data: dict):
+        reset_password = validated_data.get("reset_password")
+        can_update_password = reset_password or instance.is_superuser
+
         for key, value in validated_data.items():
-            if key == "password":
+            if key == "password" and can_update_password:
                 instance.set_password(value)
+                instance.reset_password = False
+            elif key == "password" and not can_update_password:
+                continue
             else:
                 setattr(instance, key, value)
 
