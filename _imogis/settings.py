99096120e77b30fb51e8b0b95d2e2dfa38c8d242
Jhonnatan Douglas <jhonnatanaraujodev@gmail.com>
2024-10-18 19:00:04 -0300

    fix: corrigido rota de update/delete | feat: criado o CRUD de users

--- _imogis/settings.py
+++ _imogis/settings.py
@@ -134,24 +134,16 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": """
-        django.contrib.auth.password_validation.UserAttributeSimilarityValidator
-        """,
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": """
-        django.contrib.auth.password_validation.MinimumLengthValidator
-        """,
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": """
-        django.contrib.auth.password_validation.CommonPasswordValidator
-        """,
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": """
-        django.contrib.auth.password_validation.NumericPasswordValidator
-        """,
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -185,10 +177,8 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
     ),
-    "DEFAULT_PAGINATION_CLASS": """
-    rest_framework.pagination.PageNumberPagination
-    """,
-    "PAGE_SIZE": 2,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path("accounts/", views.ReadCreateUserView.as_view()),
-    path("accounts/<int:pk>", views.ReadCreateUserView.as_view()),
+    path("users/", views.ReadCreateUserView.as_view()),
+    path("users/<uuid:pk>/", views.RetrieveUpdateDeleteUserView.as_view()),
     path(
         "login/",
         TokenObtainPairView.as_view(),
