be8896bc10f90db41e8bd0a222002389173a8ca1
Jhonnatan Douglas <jhonnatanaraujodev@gmail.com>
2024-10-18 18:17:23 -0300

    feat: configura os serializers de users

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueValidator
 from users.models import User
 
 
@@ -9,15 +9,35 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "username",
+            "first_name",
+            "last_name",
             "password",
             "email",
             "reset_password",
             "is_superuser",
             "is_active",
+            "last_login",
             "date_joined",
         ]
         extra_kwargs = {
+            "id": {"read_only": True},
             "password": {"write_only": True},
+            "username": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="A user with that username already exists.",
+                    )
+                ]
+            },
+            "email": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="user with this email already exists.",
+                    )
+                ]
+            },
         }
 
     def create(self, validated_data: dict):
